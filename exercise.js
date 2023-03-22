713c1b8ef997f03c1d55055723104be45e2f21c0
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-03-22 14:57:20 +0300

    added a function to flatten arrays

--- exercise.js
+++ exercise.js
@@ -4,3 +4,19 @@ function multiply(a,b){
 }
 const results=multiply(2,5);
 console.log(results);
+
+Arra=[1,[1,[2,3,[5,6,7]]],2];
+const arr=[1, [2, [3, 4], 5], 6];
+function flatten(arr) {
+    let flatArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        flatArr = flatArr.concat(flatten(arr[i]));
+      } else {
+        flatArr.push(arr[i]);
+      }
+    }
+   return flatArr;
+  }
+  const res=flatten(arr);
+  console.log(res);
