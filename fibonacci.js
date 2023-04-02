870162403df1548b9fccaf249d6cdebcacfd3898
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-04-02 23:35:09 +0300

    added a function

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-function printFibonacci(num){
+function printFibonacci(num,word){
     const series=[0,1];
     for(let i=2;i<=num;i++){
         let nextFibonancciNumber=series[i-2]+series[i-1];
@@ -8,7 +8,10 @@ function printFibonacci(num){
             break;
         }
     }
-    return series;
+    console.log(series);
+    word=word.toLowerCase(word);
+    
+    return word===word.split('').reverse().join('');
 }
 
-console.log(printFibonacci(10));
+console.log(printFibonacci(10,'anna'));
